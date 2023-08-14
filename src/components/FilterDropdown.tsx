a270f589c5aa845ffd3623d05e1615999f00b15c
Godbless-Abbew <godbless.abbew@amalitech.com>
2023-08-14 11:19:29 +0000

    updated the filter dropdown

--- src/components/FilterDropdown.tsx
+++ src/components/FilterDropdown.tsx
@@ -1,39 +1,63 @@
-import React, { useEffect } from 'react';
-import '../styles/filterDropdown.css';
+import React, { useState } from "react";
+import { FaAngleUp,  FaAngleDown } from "react-icons/fa";
+import "../styles/filterDropdown.css";
 
 interface FilterDropdownProps {
   regionFilter: string;
   setRegionFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const FilterDropdown: React.FC<FilterDropdownProps> = ({ regionFilter, setRegionFilter }) => {
-  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedRegion = e.target.value;
+const FilterDropdown: React.FC<FilterDropdownProps> = ({
+  regionFilter,
+  setRegionFilter,
+}) => {
+  const options = [
+    { value: "all", label: "Filter by Region" },
+    { value: "Africa", label: "Africa" },
+    { value: "Americas", label: "Americas" },
+    { value: "Asia", label: "Asia" },
+    { value: "Europe", label: "Europe" },
+    { value: "Oceania", label: "Oceania" },
+  ];
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleFilterChange = (selectedRegion: string) => {
     setRegionFilter(selectedRegion);
-    localStorage.setItem('regionFilter', selectedRegion);
+    localStorage.setItem("regionFilter", selectedRegion);
+    setIsDropdownOpen(false);
   };
 
-  useEffect(() => {
-    const savedRegionFilter = localStorage.getItem('regionFilter');
-    if (savedRegionFilter) {
-      setRegionFilter(savedRegionFilter);
-    }
-  }, [setRegionFilter]);
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
 
   return (
     <div className="filter-dropdown">
-      <div className="dropdown-container">
-        <select value={regionFilter} onChange={handleFilterChange}>
-          <option value="all" className='region'>Filter by region</option>
-          <option value="Africa">Africa</option>
-          <option value="Americas">Americas</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </select>
+      <div
+        className={`filter-header ${isDropdownOpen ? "open" : ""}`}
+        onClick={toggleDropdown}
+      >
+        <span>{regionFilter === "all" ? "Filter by Region" : regionFilter}</span>
+        <span className="dropdown-icon">
+          {isDropdownOpen ? <FaAngleUp /> : <FaAngleDown/>}
+        </span>
       </div>
+      {isDropdownOpen && (
+        <div className="options">
+          {options.map((option) => (
+            <div
+              key={option.value}
+              onClick={() => handleFilterChange(option.value)}
+              className="option"
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default FilterDropdown;
